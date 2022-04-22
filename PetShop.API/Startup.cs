1453fcdac280feb770a894fd25eaa15bd74e98cc
Anbu Mani <anbumani91@hotmail.com>
2022-04-22 18:44:42 +0530

    Fix: Username

--- PetShop.API/Startup.cs
+++ PetShop.API/Startup.cs
@@ -50,10 +50,7 @@ namespace PetShop.API
             //services.AddControllers();
             services.AddControllers();
 
-        }
-
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        } 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             
